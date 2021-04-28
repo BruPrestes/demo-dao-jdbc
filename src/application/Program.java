4bf05f276895af43266f64e8cb961b55fc85da3b
Bruno <brunnoprestes@gmail.com>
2021-04-28 19:09:16 -0300

    Ultimate

--- src/application/Program.java
+++ src/application/Program.java
@@ -21,7 +21,7 @@ public class Program {
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
-		System.out.println("=== TEST 2: seller findbyDepartment ===");
+		System.out.println("\n=== TEST 2: seller findbyDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		
@@ -29,25 +29,25 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 3: seller findAll ===");
+		System.out.println("\n=== TEST 3: seller findAll ===");
 		list = sellerDao.findAll();
 		
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
 		
-		System.out.println("=== TEST 4: seller insert ===");
+		System.out.println("\n=== TEST 4: seller insert ===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 	
-		System.out.println("=== TEST 5: seller update ===");
+		System.out.println("\n=== TEST 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
-		System.out.println("=== TEST 6: seller delete ===");
+		System.out.println("\n=== TEST 6: seller delete ===");
 		System.out.println("Enter id for delete test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
